31d59e32dcedc604fd5a6018bb32d64456904471
Bhanuka <biwanthabhanuka@gmail.com>
2024-12-27 09:50:54 +0530

    Initial Commit

---
+++ Square Game/Square_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Square_GameApp.swift
+//  Square Game
+//
+//  Created by Bhanuka  Pannipitiya  on 2024-12-27.
+//
+
+import SwiftUI
+
+@main
+struct Square_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
