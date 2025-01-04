1b72273ae9d363965e4c1961fcf9ea6f0a9a4565
Bhanuka <biwanthabhanuka@gmail.com>
2025-01-04 13:42:10 +0530

    Added main UI part

--- Square Game/ContentView.swift
+++ Square Game/ContentView.swift
@@ -1,13 +1,31 @@
 
 import SwiftUI
 
+struct Tile {
+    var color: Color
+    var isRevealed: Bool = false
+    var isMatched: Bool = false
+}
+
 struct ContentView: View {
+        @State private var grid: [[Tile]] = []
+        @State private var firstSelection: (row: Int, col: Int)? = nil
+        @State private var score: Int = 0
+        @State private var showFailure: Bool = false
+        @State private var showWin: Bool = false
+    
+        let gridSize = 3
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Memory match game")
+                .font(.system(size: 32,weight: .medium,design: .default))
+                .foregroundColor(.indigo)
+                .padding()
+            Text("Score: \(score)")
+                .font(.title)
+                .foregroundColor(.white)
+                .padding()
+
         }
         .padding()
     }
